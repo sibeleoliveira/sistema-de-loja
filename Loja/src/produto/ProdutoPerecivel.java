58bf05dc874aaaeb77133e77a5baaf4d42879183
BrennoPh <Brenno.dossantos@dcomp.ufs.br>
2025-07-19 18:29:24 -0300

    Update ProdutoPerecivel.java

--- Loja/src/produto/ProdutoPerecivel.java
+++ Loja/src/produto/ProdutoPerecivel.java
@@ -1,5 +1,14 @@
 package produto;
 
-public class ProdutoPerecivel {
+public class ProdutoPerecivel extends Produto {
+  private static final int tipo = 3;
 
+  public ProdutoPerecivel(int id, String nome, BigDecimal precoBase){
+    super.Produto(id, nome, precoBase);
+  }
+
+  //Getters
+  public int getTipo(){
+    return tipo;
+  }
 }
