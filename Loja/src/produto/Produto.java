65a0209d4a0585a40dbfa21d5594e6f1de053ee0
BrennoPh <Brenno.dossantos@dcomp.ufs.br>
2025-07-19 17:44:57 -0300

    Update Produto.java
    Criação da classe Produto:
    Contem os atributos de produto:
    -Id
    -Nome
    -Preço Base

--- Loja/src/produto/Produto.java
+++ Loja/src/produto/Produto.java
@@ -1,5 +1,43 @@
+import java.math.BigDecimal;
 package produto;
 
 public class Produto {
+  //Atributos
+  private int id;
+  private String nome;
+  private BigDecimal precoBase
 
+  //Construtor
+  public Produto(int id, String nome, BigDecimal precoBase){
+    this.id = id;
+    this.estoque = estoque;
+    this.nome = nome;
+    this.precoBase = precoBase;
+  }
+
+  //Getters
+  public int getId(){
+    return id;
+  }
+
+  public String getNome(){
+    return nome;
+  }
+
+  public BigDecimal(){
+    return precoBase;
+  }
+
+  //Setters
+  public void setId(int id){
+    this.id = id;
+  }
+
+  public void setNome(String nome){
+    this.nome = nome;
+  }
+
+  public void setPrecoBase(BigDecimal precoBase){
+    this.precoBase = precoBase;
+  }
 }
