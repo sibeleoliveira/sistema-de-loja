8c89ba8ddee260bcde930001e537202b08352982
BrennoPh <Brenno.dossantos@dcomp.ufs.br>
2025-07-19 19:15:51 -0300

    Update ConsoleMenu.java

--- Loja/src/ui/ConsoleMenu.java
+++ Loja/src/ui/ConsoleMenu.java
@@ -1,5 +1,30 @@
+
 package ui;
 
-public class ConsoleMenu {
+import cliente;
+import produto;
 
+import java.util.Scanner;
+
+public class ConsoleMenu {
+  private int input = -1;
+  
+  do{
+  
+    System.out.println("--Menu Principal--");
+    System.out.println("1 - Cadastrar Produto     2 - Alterar Produto");
+    System.out.println("3 - Cadastrar Cliente     4 - Alterar Cliente");
+    System.out.println("5 - Criar Nota de Compra  6 - Listar Notas Emitidas");
+    System.out.println("7 - Listar Produtos       8 - Listar Clientes");
+    System.out.println("0 - Sair");
+
+    if(input == 1){
+      System.out.println("
+    }
+
+  }while(input != 0);
+
+
+
+  
 }
