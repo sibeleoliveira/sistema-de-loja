9505bf8fe5e87ec74a82c16e7f8af5a53865a097
BrennoPh <Brenno.dossantos@dcomp.ufs.br>
2025-07-19 18:27:16 -0300

    Update ProdutoDigital.java

--- Loja/src/produto/ProdutoDigital.java
+++ Loja/src/produto/ProdutoDigital.java
@@ -1,5 +1,15 @@
 package produto;
 
-public class ProdutoDigital {
+public class ProdutoDigital extends Produto{
+  //Id do tipo;
+  private static final int tipo = 1;
 
+  //Construtor
+  public ProdutoDigital(int id, String nome, BigDecimal precoBase){
+    super.Produto(id, nome, precoBase);
+  }
+
+  public int getTipo(){
+    return tipo;
+  }
 }
