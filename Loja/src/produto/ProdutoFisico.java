3a367d4a3af37c8e1b07be5c08ca51eb916264d4
BrennoPh <Brenno.dossantos@dcomp.ufs.br>
2025-07-19 18:27:09 -0300

    Update ProdutoFisico.java

--- Loja/src/produto/ProdutoFisico.java
+++ Loja/src/produto/ProdutoFisico.java
@@ -1,5 +1,14 @@
 package produto;
 
-public class ProdutoFisico {
+public class ProdutoFisico extends Produto {
+  private static final int tipo = 2;
 
+  public ProdutoFisico(int id, String nome, BigDecimal precoBase){
+    super.Produto(id, nome, precoBase);
+  }
+
+  //Getters
+  public int getTipo(){
+    return tipo;
+  }
 }
