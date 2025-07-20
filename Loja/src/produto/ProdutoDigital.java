2b576ef586f6526e1d8f03fb3c1134c21c90f321
BrennoPh <Brenno.dossantos@dcomp.ufs.br>
2025-07-20 13:18:25 -0300

    Update ProdutoDigital.java

--- Loja/src/produto/ProdutoDigital.java
+++ Loja/src/produto/ProdutoDigital.java
@@ -1,12 +1,14 @@
 package produto;
 
+import java.math.BigDecimal; 
+
 public class ProdutoDigital extends Produto{
   //Id do tipo;
   private static final int tipo = 1;
 
   //Construtor
   public ProdutoDigital(int id, String nome, BigDecimal precoBase){
-    super.Produto(id, nome, precoBase);
+    super(id, nome, precoBase);
   }
 
   public int getTipo(){
