535dbdb14f94afe31850904c6d42f07bbbf19a3f
BrennoPh <Brenno.dossantos@dcomp.ufs.br>
2025-07-20 13:19:34 -0300

    Update ConsoleMenu.java

--- Loja/src/ui/ConsoleMenu.java
+++ Loja/src/ui/ConsoleMenu.java
@@ -1,45 +1,54 @@
 package ui;
 
-import dados;
+import dados.ArmazenamentoClientes;
+import dados.EstoqueProdutos;
+
+
 import java.util.Scanner;
 
 public class ConsoleMenu {
   private int input = -1;
   private ArmazenamentoClientes clientes = new ArmazenamentoClientes();
   private EstoqueProdutos produtos = new EstoqueProdutos();
+  private Scanner scanner = new Scanner(System.in);
   
-  do{
   
-    System.out.println("--Menu Principal--");
-    System.out.println("1 - Cadastrar Produto     2 - Alterar Produto");
-    System.out.println("3 - Cadastrar Cliente     4 - Alterar Cliente");
-    System.out.println("5 - Criar Nota de Compra  6 - Listar Notas Emitidas");
-    System.out.println("7 - Listar Produtos       8 - Listar Clientes");
-    System.out.println("0 - Sair");
+ 	public void play(){
+  
+		  do{
+		  
+		    System.out.println("--Menu Principal--");
+		    System.out.println("1 - Cadastrar Produto     2 - Alterar Produto");
+		    System.out.println("3 - Cadastrar Cliente     4 - Alterar Cliente");
+		    System.out.println("5 - Criar Nota de Compra  6 - Listar Notas Emitidas");
+		    System.out.println("7 - Listar Produtos       8 - Listar Clientes");
+		    System.out.println("0 - Sair");
+		    
+		    input = Integer.parseInt(scanner.nextLine()); 
 
-    if(input == 1){
-      produtos.cadastrarProduto();
-    } else if(input == 2){
+		    if(input == 1){
+		      produtos.cadastrarProduto();
+		    } else if(input == 2){
 
-    } else if(input == 3){
-      clientes.cadastrarCleinte();
-    } else if(input == 4){
+		    } else if(input == 3){
+		      clientes.cadastrarCliente();
+		    } else if(input == 4){
 
-    }else if(input == 5){
+		    }else if(input == 5){
 
-    }else if(input == 6){
-      
-    }else if(input == 7){
-      produtos.listarProdutos();
-    }else if(input == 8){
-      clientes.listarClientes();
-    } else if(input == 0){
-      System.out.println("SAINDO...");
-    }
+		    }else if(input == 6){
+		      
+		    }else if(input == 7){
+		      produtos.listarProdutos();
+		    }else if(input == 8){
+		      clientes.listarClientes();
+		    } else if(input == 0){
+		      System.out.println("SAINDO...");
+		    }
 
-  }while(input != 0);
+		  }while(input != 0);
 
-
+	}
 
   
 }
