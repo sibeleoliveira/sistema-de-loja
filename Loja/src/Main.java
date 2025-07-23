6bee07383187691e050f2f325ed349d0501525bd
Sibele Cruz <sibele.cruz@dcomp.ufs.br>
2025-07-23 10:32:49 -0300

    alteração de acordo com a interface ui

--- Loja/src/Main.java
+++ Loja/src/Main.java
@@ -1,9 +1,10 @@
-import ui.ConsoleMenu;
+import ui.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ConsoleMenu console =  new ConsoleMenu();
+		
+		Menu console =  new ConsoleMenu();
 		console.play();
 
 	}
